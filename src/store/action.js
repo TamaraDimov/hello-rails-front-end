3378520430a74c3193bc89215a0e8c2f6518b201
TamaraDimov <tamemojsoska@gmail.com>
2023-10-04 16:16:29 +0200

    fix linter error

--- src/store/action.js
+++ src/store/action.js
@@ -1,13 +1,13 @@
-export const getRandomGreeting = () => (dispatch) => {
-  fetch('/api/random_greeting')
-    .then((response) => response.json())
+export const getRandomGreeting = () => (dispatch,) => {
+  fetch('/api/random_greeting',)
+    .then((response,) => response.json(),)
     // eslint-disable-next-line no-use-before-define
-    .then((data) => dispatch(setRandomGreeting(data.greeting)))
+    .then((data,) => dispatch(setRandomGreeting(data.greeting,),),)
     // eslint-disable-next-line no-console
-    .catch((error) => console.error(error));
+    .catch((error,) => console.error(error,),);
 };
 
-export const setRandomGreeting = (greeting) => ({
+export const setRandomGreeting = (greeting,) => ({
   type: 'SET_RANDOM_GREETING',
   greeting,
 });
